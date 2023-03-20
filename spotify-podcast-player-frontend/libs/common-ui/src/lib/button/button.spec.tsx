35ddbf5821755134fe239d0f2d00408f32e9d3b8
蔡秀慧 <yuritsai@caixiuhuideMacBook-Air.local>
2023-03-20 13:35:10 +0800

    test(spotify-podcast-player-frontend): 以 ChatGPT 產生 button 元件的測試; 安裝 @babel/preset-react 修正執行錯誤

--- spotify-podcast-player-frontend/libs/common-ui/src/lib/button/button.spec.tsx
+++ spotify-podcast-player-frontend/libs/common-ui/src/lib/button/button.spec.tsx
@@ -1,10 +1,21 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Button from './button';
 
 describe('Button', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Button />);
+    const { baseElement } = render(
+      <Button onClick={() => console.log('I am a button.')} text="Buton" />
+    );
     expect(baseElement).toBeTruthy();
+  });
+
+  it('calls onClick when clicked', () => {
+    const handleClick = jest.fn();
+    const { getByText } = render(
+      <Button text="Click me" onClick={handleClick} />
+    );
+    fireEvent.click(getByText('Click me'));
+    expect(handleClick).toHaveBeenCalledTimes(1);
   });
 });
