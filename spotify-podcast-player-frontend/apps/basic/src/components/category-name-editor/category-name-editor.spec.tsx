d50ee60e6ca9b3c81209f21ba4463a21657caade
Yuri Tsai <showwell0120@gmail.com>
2023-04-18 23:43:43 +0800

    fix: for pass test

--- spotify-podcast-player-frontend/apps/basic/src/components/category-name-editor/category-name-editor.spec.tsx
+++ spotify-podcast-player-frontend/apps/basic/src/components/category-name-editor/category-name-editor.spec.tsx
@@ -4,7 +4,12 @@ import CategoryNameEditor from './category-name-editor';
 
 describe('CategoryNameEditor', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<CategoryNameEditor />);
+    const { baseElement } = render(
+      <CategoryNameEditor
+        categoryName={{ emoji: '', text: '' }}
+        onChange={() => {}}
+      />
+    );
     expect(baseElement).toBeTruthy();
   });
 });
