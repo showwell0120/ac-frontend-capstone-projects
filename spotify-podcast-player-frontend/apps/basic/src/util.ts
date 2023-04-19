c0131e43c236edc36ac56aa693222f0ba4bc5a57
Yuri Tsai <showwell0120@gmail.com>
2023-04-20 00:19:49 +0800

    feat: add greeting util

--- spotify-podcast-player-frontend/apps/basic/src/util.ts
+++ spotify-podcast-player-frontend/apps/basic/src/util.ts
@@ -11,3 +11,15 @@ export function splitCategoryName(categoryName: string): CategoryName {
 export function mergeCategoryName(categoryName: CategoryName): string {
   return Object.values(categoryName).join(':');
 }
+
+export function getGreeting() {
+  const time = new Date().getHours();
+
+  if (time >= 5 && time < 12) {
+    return '早安';
+  } else if (time >= 12 && time < 18) {
+    return '午安';
+  } else {
+    return '晚安';
+  }
+}
