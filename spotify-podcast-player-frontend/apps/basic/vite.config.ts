08d85ef6eff289def450852655c90dccc0ff69de
Yuri Tsai <yuri.hh.tsai@viewsonic.com>
2023-04-23 19:20:57 +0800

    fix: reset vite config

--- spotify-podcast-player-frontend/apps/basic/vite.config.ts
+++ spotify-podcast-player-frontend/apps/basic/vite.config.ts
@@ -46,10 +46,4 @@ export default defineConfig({
     environment: 'jsdom',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
   },
-
-  build: {
-    rollupOptions: {
-      external: ['emoji-picker-react'],
-    },
-  },
 });
