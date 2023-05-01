b3fc1397cdb9b8ac655c8fd0cfc26ac1bed75e95
Yuri Tsai <showwell0120@gmail.com>
2023-05-01 14:38:39 +0800

    add svgr

--- alpha-cast-frontend/src/App.tsx
+++ alpha-cast-frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { ReactComponent as ViteLogo } from '../public/vite.svg'
 import './App.css'
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <ViteLogo className="logo" width={"6em"} />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
