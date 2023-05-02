908d02a280ee6bdd80d9a45eb3e16d290aa151c3
Yuri Tsai <showwell0120@gmail.com>
2023-05-02 21:07:54 +0800

    rename repo folder, add sass, add svgr

--- alphacast-frontend/src/App.tsx
+++ alphacast-frontend/src/App.tsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import { ReactComponent as ViteLogo } from '../public/vite.svg'
+
 import './App.css'
+import styles from './App.module.scss';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +12,7 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <ViteLogo className="logo" width={"6em"} />
+          <ViteLogo className={styles.logo} width={"6em"} />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
